28b4274d877847b8ff2760e7d49ce4913651e57b
Hai Nguyen <hai5nguy@gmail.com>
2017-11-17 16:40:33 -0500

    BackboneJS pattern

--- js/app-new-new.js
+++ js/app-new-new.js
@@ -1,132 +1,121 @@
-$(function() {
-	var model = {
-		init: function () {
-			this.generateCats();
-			this.modifyCatPrototype();
-		},
+//////////////////////////////////////////////////////////////////////////////
+//app.js - this resembles the "octopus"
+function App() {
+	var currentActiveCat;
 
-		catCollection: [
-			{
-				"name": "Wepsi",
-				"imgSrc": "img/catphoto.jpg"
-			},
-			{
-				"name": "Cuddles",
-				"imgSrc": "img/catphoto2.jpg"
-			},
-			{
-				"name": "Kitty",
-				"imgSrc": "img/catphoto3.jpg"
-			},
-			{
-				"name": "Pepsi",
-				"imgSrc": "img/catphoto4.jpg"
-			},
-			{
-				"name": "CutiePie",
-				"imgSrc": "img/catphoto5.jpg"
-			}
-		],
+	var catData = [{
+		name: 'Wepsi',
+		imgSrc: 'img/catphoto.jpg'
+	}, {
+		name: 'Cuddles',
+		imgSrc: 'img/catphoto2.jpg'
+	}, {
+		name: 'Kitty',
+		imgSrc: 'img/catphoto3.jpg'
+	}, {
+		name: 'Pepsi',
+		imgSrc: 'img/catphoto4.jpg'
+	}, {
+		name: 'CutiePie',
+		imgSrc: 'img/catphoto5.jpg'
+	}];
 
-		kittyObjects: [],
+	var $catDisplay = $('#cat-display');
+	var $catList = $('#cat-list');
 
-		CatModel: function(name, imgSrc) {
-			let self = this;
-			self.name = name;
-			self.counter = 0;
-			self.imgSrc = imgSrc;
-		},
+	catData.forEach(function (cat) {
+		var model = new CatModel(cat);
 
-		modifyCatPrototype: function() {
-			this.CatModel.prototype.clickyKitty = function() {
-				this.counter++;
-				octopus.updateCatDisplayDiv(this);
-			}
-		},
+		var display = new CatDisplayView({ model: model });
+		$catDisplay.append(display.render().$el);
 
-		generateCats: function() {
-			model.catCollection.forEach(function(data) {
-				var kitty = new model.CatModel(data.name, data.imgSrc);
-				model.kittyObjects.push(kitty);
-			});
-		}
+		var item = new CatListItemView({ model: model });
+		item.onClick(function () {
+			if (currentActiveCat) { currentActiveCat.setInactive() }
+			currentActiveCat = display;
+			currentActiveCat.setActive();
+		});
+		$catList.append(item.render().$el);
 
-	};
+	});
+}
+//////////////////////////////////////////////////////////////////////////////
+//cat-model.js
+var CatModel = function (options) {
+	var self = this;
+	self.name = options.name;
+	self.imgSrc = options.imgSrc;
+	self.counter = 0;
 
-	var octopus = {
-		init: function () {
-			model.init();
-			catListView.init();
-			catDisplayView.init();
-		},
+	self.increment = function () {
+		self.counter++;
+		self.triggerChange();
+	}
 
-		getAllKitties: function () {
-			return model.kittyObjects;
-		},
+	//this part of the model is normally part of backbonejs code, not your app.  this is here to show how backbone does events
+	self._changeListeners = undefined;
+	
+	self.addChangeListener = function (listener) {
+		self._changeListeners = listener
+	}
+	self.triggerChange = function () {
+		self._changeListeners.call(self);
+	}
+}
+//////////////////////////////////////////////////////////////////////////////
+//cat-listitem-view.js
+var CatListItemView = function (options) {
+	var self = this;
+	self.model = options.model;
 
-		updateCatListItem: function (data, li) {
-			data.listItem = li;
-			data.listItem.click(function(e) { 
-				catDisplayView.displayCatDiv(data);
-			});
-		},
+	self.render = function () {
+		var template = '<li>' + self.model.name + '</li>'
+		var $el = $(template)
+		$el.click(function () {
+			self.onClickHander();
+		});
+		self.$el = $el;
+		return self;
+	}
 
-		updateCatDisplayData: function (data, div) {
-			data.catDisplayDiv = div;
-			data.catDisplayDiv.click(function(e) {
-				data.clickyKitty();
-			});
-		},
+	self.onClick = function (handler) {
+		self.onClickHander = handler;
+	}
+};
 
-		updateCatDisplayDiv: function(data) {
-			catDisplayView.updateCatCounter(data);
-		}
-	};
+//////////////////////////////////////////////////////////////////////////////
+//cat-display-view.js
+function CatDisplayView (options) {
+	var self = this;
+	self.model = options.model;
 
-	var catListView = {
-		init: function () {
-			this.render();
-		},
+	self.model.addChangeListener(function () {
+		self.$el.find('span').html(self.model.counter);
+	});
 
-		render: function() {
-			var htmlStr = '';
-			octopus.getAllKitties().forEach(function(data) {
-				htmlStr = $('<li>' + data.name + '</li>');
-				$('#cat-list').append(htmlStr);
-				octopus.updateCatListItem(data, htmlStr);
-			});
-		}
-	};
+	self.render = function () {
+		var template = '<div class="kitty" style="display: none;"><h1>' + self.model.name + '</h1><figure><img src="' + self.model.imgSrc + '"><figcaption>Click Count: <span>' + self.model.counter + '</span></figcaption></figure></div>';
+		var $el = $(template);
+		
+		$el.find('img').click(function () {
+			self.model.increment();
+		});
 
-	var catDisplayView = {
-		init: function () {
-			this.render();
-		},
+		self.$el = $el;
 
-		render: function() {
-			var htmlStr = '';
-			octopus.getAllKitties().forEach(function(data) {
-				htmlStr = $('<div class="kitty"><h1>' + data.name + '</h1><figure><img src="' + data.imgSrc + '"><figcaption>Click Count: <span>0</span></figcaption></figure></div>');
-				$('#cat-display').append(htmlStr);
-				octopus.updateCatDisplayData(data, htmlStr);
-				catDisplayView.hideKittyDivs();
-			});
-		},
+		return self;
+	}
 
-		hideKittyDivs: function() {
-			$('.kitty').hide();
-		},
+	self.setActive = function () {
+		self.$el.show();
+	}
 
-		displayCatDiv: function(data) {
-			catDisplayView.hideKittyDivs();
-			data.catDisplayDiv.show();
-		},
+	self.setInactive = function () {
+		self.$el.hide();
+	}
+}
 
-		updateCatCounter: function(data) {
-			data.catDisplayDiv.find('span').text(data.counter);
-		}
-	};
-
-	octopus.init();
-
+//normally this would belong in app.js
+$(function () {
+	App();
 });
